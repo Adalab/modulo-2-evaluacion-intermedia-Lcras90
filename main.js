78b70c37cd38a982da56ae43f72ac0bf4247484f
Lucia Cano <luciacanoras@gmail.com>
2021-05-12 10:29:22 +0200

    try to function

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 "use strict";
 let user = document.querySelector('input[name="inpuT"]');
 const buttoN = document.querySelector(".js-button");
+let count = 0;
+let max = getRandomNumber(100);
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-console.log(getRandomNumber(100));
+console.log(max);
 
 function handlerInput(event) {
-  let value = user;
-  if (value < getRandomNumber(max)) {
+  event.preventDefault();
+  let value = parseInt(user.value);
+  if (value < max) {
     const p1 = document.querySelector(".js-paragraph1");
     p1.innerText = "Demasiado bajo";
-  } else if (value > getRandomNumber(max)) {
+  } else if (value > max) {
     p1.innerText = "Demasiado alto";
-  } else if (value === getRandomNumber(max)) {
+  } else if (value === max) {
     p1.innerText = "¡¡¡Has ganado campeona!!!";
   }
+}
+
+function countClick() {
+  count += 1;
+  const p2 = document.querySelector(".js-");
+  p2.innerText = "";
 }
 
 buttoN.addEventListener("click", handlerInput);
